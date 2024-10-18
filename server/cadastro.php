d472b7064e59e25efdca605c9e53f037a79bd9c0
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2024-10-18 20:02:26 -0300

    Inicio da tela de Cadastro

---
+++ server/cadastro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="Tela de Sucesso">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de Sucesso - Teste</title>
+</head>
+<body>
+    <p> 
+        Nome: <?php echo $_POST["nome"]; ?><br>
+        Email: <?php echo $_POST["email"]; ?><br>
+        Senha: <?php echo $_POST["senha"]; ?><br>
+        Endereço: <?php echo $_POST["endereco"]; ?>
+    </p>
+</body>
+</html>
