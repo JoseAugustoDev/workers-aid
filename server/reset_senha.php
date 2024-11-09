62a22201387302f824bbf9db808905f022c2ecd6
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2024-11-08 21:39:45 -0300

    CRUD Redefinição de Senha

--- server/reset_senha.php
+++ server/reset_senha.php
@@ -5,23 +5,41 @@
     $dbname = "dados";
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+    
     if (!$conn) {
-        die("Falha na conexao: " . mysqli_connect_error());
+        die("Falha na conexão: " . mysqli_connect_error());
     }
 
-    $nome = mysqli_real_escape_string($conn, $_POST["email"]);
-    $email = mysqli_real_escape_string($conn, $_POST["redefinirsenha"]);
-    $senha = mysqli_real_escape_string($conn, $_POST["confirmacaodasenha"]);
 
-    $sql = "SELECT";
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $senha_antiga = mysqli_real_escape_string($conn, $_POST["senhaAntiga"]);
+    $senha_nova = mysqli_real_escape_string($conn, $_POST["senhanova"]);
+    $confirmacao = mysqli_real_escape_string($conn, $_POST["confirmacaodasenha"]);
 
-    if ($conn->query($sql) === TRUE) {
-        echo "Redefinido com Sucesso";
+    if ($senha_nova === $confirmacao) {
+        
+        $usuario_query = "SELECT id_cliente FROM clientes WHERE email = '$email' AND senha = '$senha_antiga'";
+        $usuario_result = $conn->query($usuario_query);
+
+        if ($usuario_result->num_rows > 0) {
+
+            $usuario = $usuario_result->fetch_assoc();
+            $id_cliente = $usuario['id_cliente'];
+
+            
+            $redefinir_query = "UPDATE clientes SET senha = '$senha_nova' WHERE id_cliente = '$id_cliente'";
+            if ($conn->query($redefinir_query) === TRUE) {
+                echo "Senha redefinida com sucesso";
+            } else {
+                echo "Erro ao redefinir a senha: " . $conn->error;
+            }
+        } else {
+            echo "Usuário não encontrado ou senha antiga incorreta";
+        }
     } else {
-        echo "Erro: " . $sql . "<br>" . $conn->error;
+        echo "A nova senha e a confirmação não coincidem";
     }
 
+   
     $conn->close();
-
 ?>
